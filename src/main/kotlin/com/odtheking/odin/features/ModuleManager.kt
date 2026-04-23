5063070a167ceb1b89054e0ffdb78d74b8ee34b6
morningsoftware <morningsoftware@proton.me>
2026-04-23 01:33:29 -0400

    Add EasyEtherwarp & "Always Show" option for Etherwarp Overlay.
    EasyEtherwarp enables quick teleportation with AOTV, by mimicking "Sneak + RightClick" upon LeftClick. As a result, it makes sense to also add an option to always show the Etherwarp prediction when holding AOTV, even when not sneaking. Hence, the option was added.

--- src/main/kotlin/com/odtheking/odin/features/ModuleManager.kt
+++ src/main/kotlin/com/odtheking/odin/features/ModuleManager.kt
@@ -73,7 +73,7 @@ object ModuleManager {
 
             //skyblock
             ChatCommands, NoCursorReset, Ragnarock, SpringBoots, WardrobeKeybinds, PetKeybinds, AutoSprint,
-            CommandKeybinds, SlotBinds, Splits,
+            CommandKeybinds, SlotBinds, Splits, EasyEtherwarp,
 
             // nether
             SupplyHelper, BuildHelper, RemovePerks, NoPre, PearlWaypoints, FreshTools, KuudraInfo, Misc, Vesuvius
---
+++ src/main/kotlin/com/odtheking/odin/features/impl/skyblock/EasyEtherwarp.kt
@@ -0,0 +1,55 @@
+package com.odtheking.odin.features.impl.skyblock
+
+import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
+import com.odtheking.odin.events.TickEvent
+import com.odtheking.odin.events.core.on
+import com.odtheking.odin.features.Module
+import com.odtheking.odin.utils.isEtherwarpItem
+import com.odtheking.odin.utils.skyblock.LocationUtils
+
+object EasyEtherwarp : Module(
+    name = "EasyEtherwarp",
+    description = "Left-clicking with AOTV activates Etherwarp."
+) {
+    private enum class EtherwarpState { IDLE, SNEAKING, INTERACTING }
+    private var state = EtherwarpState.IDLE
+    private var attackKeyWasDown = false
+    private var lastTeleportTime = 0L
+    private var cooldown = 500L
+
+    init {
+        on<TickEvent.End> {
+            if (!LocationUtils.isInSkyblock) return@on
+            val player = mc.player ?: return@on
+
+            when (state) {
+                EtherwarpState.SNEAKING -> {
+                    mc.gameMode?.useItem(player, player.usedItemHand)
+                    state = EtherwarpState.INTERACTING
+                }
+                EtherwarpState.INTERACTING -> {
+                    mc.options.keyShift.isDown = false
+                    state = EtherwarpState.IDLE
+                }
+                EtherwarpState.IDLE -> {}
+            }
+
+            if (state == EtherwarpState.IDLE) {
+                val held = player.mainHandItem
+                if (held.isEtherwarpItem() == null) return@on
+                val attackKeyIsDown = mc.options.keyAttack.isDown
+                val curTime = System.currentTimeMillis()
+
+                // Do not allow holding left-click for multiple TPs, and only allow one TP every 500ms.
+                // Without either of these checks, there is a high chance for AC flags.
+                // TODO: Add an adjustable cooldown.
+                if (attackKeyIsDown && curTime - lastTeleportTime >= cooldown && !attackKeyWasDown) {
+                    lastTeleportTime = curTime
+                    mc.options.keyShift.isDown = true
+                    state = EtherwarpState.SNEAKING
+                }
+                attackKeyWasDown = attackKeyIsDown
+            }
+        }
+    }
+}
